b42aa02fd0d7aac29f4463f18394ceb43b5a9534
Curtis <50036729@belfastmet.ac.uk>
2024-11-07 15:19:33 +0000

    Add conditional statements lab

--- Functions Lab/Exercise 2.py
+++ Functions Lab/Exercise 2.py
@@ -1,3 +1,10 @@
+def employee_pay(): 1 usage*
+    hourly_rate = float(input(f'Please enter your hourly rate, e.g. 10.59: ')
+    num_hours_worked = input(f'Please enter your number of hours worked, e.g. 21.5'))
+    emp_pay = hourly_rate * num_hours_worked
+    emp_pay = f'£ {emp_pay}'
+    return emp_pay
+
 """
 list_of_ages = [20,21.24,13,15,19,22]
 1. Add all ages together
@@ -7,7 +14,7 @@ list_of_ages = [20,21.24,13,15,19,22]
 
 """
 def average_age(ages_list:list): 1 usage
-""""
+    """"
 :param ages_list: should a list of ages, expecting the data type
     :return: average age in float
 """
@@ -24,3 +31,6 @@ print(f'The average age is {abg_age} years')
 #Test employee_pay code
 empy_pay = employee_pay()
 print(f'Employee Pay is {empy_pay}')
+
+
+
